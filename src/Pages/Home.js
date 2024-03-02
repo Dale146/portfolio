05ca63d8247e2b562400c44940e3417dc4b4b5cb
Dale146 <150839726+Dale146@users.noreply.github.com>
2024-03-02 15:21:56 -0800

    update background

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import pixel from "../images/transparent_2024-03-01T00-27-27.png";
-import sky from "../images/sky.jpg";
+import sky1 from "../images/1.png";
+import land from "../images/2.png";
+import lake from "../images/3.png";
+import scape from "../images/4.png";
+import dog from "../images/Idle.png";
 
 const Home = () => {
   // 初始化 dot 的初始位置
@@ -10,9 +14,12 @@ const Home = () => {
     <div className="home">
       <div className="container">
       <div>
-
-      <img className="sky" src={sky} alt="sky"/>
+      <img className="sky" src={sky1} alt="sky"/>
       </div>
+      <img className="land" src={land}/>
+      <img className="lake" src={lake}/>
+      <img className="scape" src={scape}/>
+      <div className="dog"></div>
       </div>
     </div>
   );
