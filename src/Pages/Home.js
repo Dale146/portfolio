93c163940cbb802e8560cf106d1231e401e6dde2
Dale146 <150839726+Dale146@users.noreply.github.com>
2024-03-04 20:27:39 -0800

    update on multiple arrays

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "../components/modal";
 import Typewriter from "../components/typeWriter";
 import sky1 from "../images/1.png";
@@ -6,64 +6,81 @@ import land from "../images/2.png";
 import lake from "../images/3.png";
 import scape from "../images/4.png";
 import dog from "../images/Idle.png";
+import data from "../data.json";
+
+
 
 const Home = () => {
-  const [dogString, setDogString] = useState([
-    "This is paragraph 1",
-    "This is paragraph 2",
-    "This is paragraph 3",
-  ]);
-  
-  const [modalText, setModalText] = useState(dogString[0]);
+  const dogString = data.dog;
+  const secondString = data.secondString;
 
-
+  // select the array thats used
+  const [selectString, setSelectString] = useState([]);
+  // for choosing the array number
   const [currentIndex, setCurrentIndex] = useState(0);
+  // made for change the text in the modal
+  const [modalText,setModalText] = useState(selectString[0]);
 
+  // change the text inside the modal
+  useEffect(() => {
+
+  },[modalText]);
+
+  // counting the array.length and make the index always smaller to it.
+  // set the indexNumber each time clicked the button
+  // set the modalText to to the srray index number.
   const showNextParagraph = () => {
-    if (currentIndex < dogString.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-      setModalText(dogString[currentIndex]);
-      console.log(modalText)
-      
-    }else{
-      console.log(dogString[currentIndex])
+    if (currentIndex < selectString.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+      setModalText(selectString[currentIndex + 1]);
+    } else {
+      console.log(selectString[currentIndex]);
     }
   };
 
-  // check if the modal is open
+  // Check if the modal is open
   const [isModalOpen, setModalOpen] = useState(false);
 
-  // onclick function to make modal open
-  const handleOpenModal = () => {
+  // OnClick function to open the modal
+  const handleOpenModal = (array) => {
+    setSelectString(array);
+    setCurrentIndex(0);
+    setModalText(array[0]);
     setModalOpen(true);
-    
-    
   };
 
-  // onclick function to make modal close
+  // OnClick function to close the modal
   const handleCloseModal = () => {
     setModalOpen(false);
-   
+    setCurrentIndex(0);
+    
   };
 
   return (
     <div className="home">
+      <button className="other" onClick={() => handleOpenModal(secondString)}>
+        other
+      </button>
       <div className="container">
         <div>
           <img className="sky" src={sky1} alt="sky" />
         </div>
-        <img className="land" src={land} />
-        <img className="lake" src={lake} />
-        <img className="scape" src={scape} />
+        <img className="land" src={land} alt="land" />
+        <img className="lake" src={lake} alt="lake" />
+        <img className="scape" src={scape} alt="scape" />
+
         <div className="dog">
-          <img src={dog} alt="dog" onClick={handleOpenModal} />
+          <img
+            src={dog}
+            alt="dog"
+            onClick={() => handleOpenModal(dogString)}
+          />
         </div>
         <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-          <button onClick={showNextParagraph}>1111</button>
+          <button onClick={showNextParagraph}>111</button>
           <p>
-            <Typewriter text={modalText}/>
+            <Typewriter key={modalText} text={modalText} />
           </p>
-          
         </Modal>
       </div>
     </div>
