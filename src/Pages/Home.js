a075d1357fea13a81cb5fcf7b7ee9f7099afb5d3
Dale146 <150839726+Dale146@users.noreply.github.com>
2024-03-04 22:15:25 -0800

    update cases

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,14 +6,25 @@ import land from "../images/2.png";
 import lake from "../images/3.png";
 import scape from "../images/4.png";
 import dog from "../images/Idle.png";
+// import json data
 import data from "../data.json";
 
 
 
 const Home = () => {
-  const dogString = data.dog;
-  const secondString = data.secondString;
+  // import array from json file
+  const dogString = data.dog.paragraphs;
+  const secondString = data.secondString.paragraphs;
+  const dogName = data.dog.name;
+  const secondName = data.secondString.name;
+  const dogMoreString = data.dog.more;
+  const secondParagraph = data.secondString.more;
 
+  // select the name thats used
+  const [selectName, setSelectName] = useState("");
+  // trigger is same function triggered twice
+  const [dogTrigger, setDogTrigger] = useState(false);
+  const [secondTrigger, setSecondTrigger] = useState(false);
   // select the array thats used
   const [selectString, setSelectString] = useState([]);
   // for choosing the array number
@@ -42,23 +53,57 @@ const Home = () => {
   const [isModalOpen, setModalOpen] = useState(false);
 
   // OnClick function to open the modal
-  const handleOpenModal = (array) => {
+  // the parameter are the arrays and the names
+  const handleOpenModal = (array, name, array2) => {
+    // change the name of the array
+    setSelectName(name)
+    // change the array
     setSelectString(array);
+    // set the index to 0 just to reset
     setCurrentIndex(0);
     setModalText(array[0]);
     setModalOpen(true);
+    console.log(dogTrigger)
+    // switch by name of the array
+    switch (name){
+      case "dog":
+        if (dogTrigger === false){
+           setDogTrigger(true);
+        }else{
+          setCurrentIndex(0);
+          setModalText(array2[0]);
+          setSelectString(array2);
+        };
+        break;
+        
+      case "this is the name":
+        if(secondTrigger === false){
+          setSecondTrigger(true);
+
+        }else{
+          setCurrentIndex(0);
+          setModalText(array2[0]);
+          setSelectString(array2);
+        }
+        break;
+        
+        default:
+        return null;
+    };
   };
 
   // OnClick function to close the modal
   const handleCloseModal = () => {
     setModalOpen(false);
     setCurrentIndex(0);
+    console.log(dogTrigger)
     
   };
 
   return (
     <div className="home">
-      <button className="other" onClick={() => handleOpenModal(secondString)}>
+      {/* give the array and the name */}
+      <button className="other" onClick={() => handleOpenModal(secondString, secondName, secondParagraph)}>
         other
       </button>
       <div className="container">
@@ -73,12 +118,15 @@ const Home = () => {
           <img
             src={dog}
             alt="dog"
-            onClick={() => handleOpenModal(dogString)}
+            onClick={() => handleOpenModal(dogString, dogName, dogMoreString)}
           />
         </div>
         <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
           <button onClick={showNextParagraph}>111</button>
+          {/* use select name */}
+          <p>{selectName}</p>
           <p>
+            {/* use modal text */}
             <Typewriter key={modalText} text={modalText} />
           </p>
         </Modal>
