5dd58ef43576416970e2e22b0f3bb9b6e66e7656
Dale146 <150839726+Dale146@users.noreply.github.com>
2024-03-18 15:03:08 -0700

    slidedown animation

--- src/components/mobile-projects.js
+++ src/components/mobile-projects.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import upArrow from "../images/icons/iconmonstr-caret-up-circle-lined.svg";
 
 
 const MobileProjects = () => {
@@ -22,25 +23,51 @@ const MobileProjects = () => {
   const projects = fetchedData.projects || [];
   console.log(projects)
 
+    const [hiddenContent, setHiddenContent] = useState(false)
+    const displayHiddenContent = () => {
+      setHiddenContent(!hiddenContent);
+    }
   return (
     <div className="mobile-projects">
         {projects.map((project, index) => {
         const projectKeys = Object.keys(project);
 
         return (
-          <div key={index}>
+          <div className="mobile-container" key={index}>
             {projectKeys.map((key) => (
+              <div className="cover-container" onClick={displayHiddenContent}>
               <div key={key} className="box">
               <img src={project[key].imgSrc} alt={project[key].imgAlt} />
-              <h4>{project[key].title}</h4>
+              <h3>{project[key].title}</h3>
               <ul>
                 {project[key].mainTools && project[key].mainTools.map((tool,i) => (
                   <li key={i}>{tool}</li>
                 ))}
               </ul>
               <span>{project[key].year}</span>
-              
               </div>
+             
+              <div key={key} className={`hidden-content ${hiddenContent ? "show slide-in" : "hide"}`}>
+                <h4>Project Description</h4>
+                <p>{project[key].description}</p>
+                <h4>Highlights</h4>
+                  <ul>
+                  {project[key].highlight && project[key].highlight.map((highlight,i) => (
+                    <li key={i}>{highlight}</li>
+                  ))}
+                  </ul>
+                <h4>Any Insights?</h4>
+                  <ul>
+                  {project[key].insight && project[key].insight.map((insight,i) => (
+                    <li key={i}>{insight}</li>
+                  ))}
+                  </ul>
+                </div>
+                    <section className="up-btn">
+                    <img src={upArrow} alt="uparrow"/>
+                    </section>
+
+                </div>
               ))}
           </div>
           
