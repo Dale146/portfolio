ebd1019a04815291efe6b570f65d5494dc219086
Dale146 <150839726+Dale146@users.noreply.github.com>
2024-03-20 19:26:30 -0700

    finished mobile projects style

--- src/components/mobile-projects.js
+++ src/components/mobile-projects.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import upArrow from "../images/icons/iconmonstr-caret-up-circle-lined.svg";
+
 
 
 const MobileProjects = () => {
@@ -21,63 +21,46 @@ const MobileProjects = () => {
 
   // Ensure project is an array
   const projects = fetchedData.projects || [];
-  console.log(projects)
 
-    const [hiddenContent, setHiddenContent] = useState(false)
-    const displayHiddenContent = (index, title) => {
-      setHiddenContent(!hiddenContent);
-      console.log(title)
-    }
     
   return (
-    
     <div className="mobile-projects">
       <h1>Projects</h1>
-        {projects.map((project, index) => {
+      {projects.map((project, index) => {
         const projectKeys = Object.keys(project);
-
+  
         return (
           <div className="mobile-container" key={index}>
             {projectKeys.map((key) => (
-              <div className="cover-container" onClick={displayHiddenContent}>
-              <div key={key} className="box">
-              <img src={project[key].imgSrc} alt={project[key].imgAlt} />
-              <h3>{project[key].title}</h3>
-              <ul>
-                {project[key].mainTools && project[key].mainTools.map((tool,i) => (
-                  <li key={i}>{tool}</li>
-                ))}
-              </ul>
-              <span>{project[key].year}</span>
+              <div className="cover-container" key={`${index}-${key}`}>
+                <div className="card">
+                  <div className="logoBox">
+                    <img src={project[key].logo} alt={project[key].imgAlt} />
+                  </div>
+                  <div className="prj-content">
+                    <div className="prj-image">
+                      <img src={project[key].imgSrc} alt={project[key].imgAlt}/>
+                    </div>
+                    <div className="prj-details">
+                      <h2>{project[key].title}<br/>
+                      <span>{project[key].shortdes}</span></h2>
+                      <div className="kitsData">
+                        {project[key].mainTools && project[key].mainTools.map((tool, i) => (
+                          <h3 key={i}>{tool}</h3>
+                        ))}
+                      </div>
+                      <div className="actionBtn">
+                        <button>GitHub</button>
+                        <button>Live Site</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-             
-              <div key={key} className={`hidden-content ${hiddenContent ? "show slide-in" : "hide"}`}>
-                <h4>Project Description</h4>
-                <p>{project[key].description}</p>
-                <h4>Highlights</h4>
-                  <ul>
-                  {project[key].highlight && project[key].highlight.map((highlight,i) => (
-                    <li key={i}>{highlight}</li>
-                  ))}
-                  </ul>
-                <h4>Any Insights?</h4>
-                  <ul>
-                  {project[key].insight && project[key].insight.map((insight,i) => (
-                    <li key={i}>{insight}</li>
-                  ))}
-                  </ul>
-                </div>
-                    <section className="up-btn">
-                    <img src={upArrow} alt="uparrow"/>
-                    </section>
-
-                </div>
-              ))}
+            ))}
           </div>
-          
         );
       })}
-
       <button>Home</button>
     </div>
   );
